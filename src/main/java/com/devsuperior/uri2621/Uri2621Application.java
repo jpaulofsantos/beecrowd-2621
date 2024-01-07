56b6de2fe8478ca7f05ddf631d06c65468549c47
JoaoPaulo <jpaulofsantos@gmail.com>
2024-01-07 13:52:04 -0300

    JPQL product min search

--- src/main/java/com/devsuperior/uri2621/Uri2621Application.java
+++ src/main/java/com/devsuperior/uri2621/Uri2621Application.java
@@ -1,5 +1,6 @@
 package com.devsuperior.uri2621;
 
+import com.devsuperior.uri2621.dto.ProductMinDTO;
 import com.devsuperior.uri2621.projection.ProductMinProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.devsuperior.uri2621.repositories.ProductRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Uri2621Application implements CommandLineRunner {
@@ -23,21 +25,30 @@ public class Uri2621Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		List<ProductMinProjection> listProjection = repository.searchSQL();
+		List<ProductMinProjection> listProjection = repository.searchSQL(10, 300, "E");
+		List<ProductMinDTO> list = listProjection.stream().map(x-> new ProductMinDTO(x)).collect(Collectors.toList());
+		List<ProductMinDTO> listProjectionDTO = repository.searchJPQL(10, 400, "E");
+
 
 		System.out.println("");
-		System.out.println("------SQL------");
+		System.out.println("------SQL-PROJ------");
 
 		for (ProductMinProjection item: listProjection){
+			System.out.println(item.getName());
+		}
+
+		System.out.println("");
+		System.out.println("------SQL-DTO------");
+
+		for (ProductMinDTO item: list){
 			System.out.println(item.getName());
 		}
 
 		System.out.println("");
 		System.out.println("------JPQL------");
 
-		for (ProductMinProjection item: listProjection){
+		for (ProductMinDTO item: listProjectionDTO){
 			System.out.println(item.getName());
 		}
-		
 	}
 }
--- src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
+++ src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.devsuperior.uri2621.repositories;
 
+import com.devsuperior.uri2621.dto.ProductMinDTO;
 import com.devsuperior.uri2621.projection.ProductMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -12,8 +13,13 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Query(nativeQuery = true, value = "SELECT products.name from products " +
             "INNER JOIN providers ON products.id_providers = providers.id " +
-            "WHERE products.amount BETWEEN 10 AND 20 " +
-            "AND providers.name LIKE 'P%'")
-    List<ProductMinProjection> searchSQL();
+            "WHERE products.amount BETWEEN :min AND :max " +
+            "AND providers.name LIKE CONCAT(:letter, '%')")
+    List<ProductMinProjection> searchSQL(Integer min, Integer max, String letter);
+
+    @Query("SELECT new com.devsuperior.uri2621.dto.ProductMinDTO(obj.name) FROM Product obj " +
+            "WHERE obj.amount BETWEEN :min AND :max " +
+            "AND obj.provider.name LIKE CONCAT(:letter, '%')")
+    List<ProductMinDTO> searchJPQL(Integer min, Integer max, String letter);
 
 }
